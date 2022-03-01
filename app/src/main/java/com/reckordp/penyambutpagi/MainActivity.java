cf612b64340cd3bf4f3b8c847911d7f98674754e
Reckordp <rmuhammad134@gmail.com>
2022-03-01 11:57:00 +0700

    Menambahkan alert pembuat

--- app/src/main/java/com/reckordp/penyambutpagi/MainActivity.java
+++ app/src/main/java/com/reckordp/penyambutpagi/MainActivity.java
@@ -3,9 +3,13 @@ package com.reckordp.penyambutpagi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String GITHUB_PROFILE = "https://github.com/Reckordp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,10 @@ public class MainActivity extends AppCompatActivity {
         pembuat.setMessage(R.string.message);
         pembuat.setTitle(R.string.title_message);
         pembuat.setPositiveButton(android.R.string.ok, (dialog, which) -> {});
-        pembuat.setNegativeButton(R.string.lebih_lanjut, (dialog, which) -> {});
+        pembuat.setNegativeButton(R.string.lebih_lanjut,
+                (dialog, which) -> startActivity(
+                        new Intent(Intent.ACTION_VIEW, Uri.parse(GITHUB_PROFILE))
+                ));
         pembuat.show();
     }
 }
